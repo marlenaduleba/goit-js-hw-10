2a3a9e6d600440ab5630d622ea1158c230341906
Marlena Dulęba <marlena_duleba@o2.pl>
2022-12-29 16:31:11 +0100

    API path fixed

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,8 @@
 
 export function fetchCountries(name) {
-    return fetch(`https://restcountries.com/v2/all?fields=name,capital,population,flags,languages`).then(
+    const params = "fields=name,capital,population,flags,languages"
+
+    return fetch(`https://restcountries.com/v3.1/name/${name}?${params}`).then(
         (response) => {
             if (!response.ok) {
                 throw new Error(response.status);
