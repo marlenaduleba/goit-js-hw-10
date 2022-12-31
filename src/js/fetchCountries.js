63f90c39bf38b62bce7c63e1b8336cd589c14ac2
Marlena Dulęba <marlena_duleba@o2.pl>
2022-12-31 13:21:34 +0100

    spaces deleted

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,13 +1,12 @@
-
 export function fetchCountries(name) {
-    const params = "fields=name,capital,population,flags,languages"
+  const params = 'fields=name,capital,population,flags,languages';
 
-    return fetch(`https://restcountries.com/v3.1/name/${name}?${params}`).then(
-        (response) => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
-            return response.json();
-        }
-    )
-} 
+  return fetch(`https://restcountries.com/v3.1/name/${name}?${params}`).then(
+    response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    }
+  );
+}
